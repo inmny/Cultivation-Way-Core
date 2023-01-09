c2b35585e943c4e745319d46152109b6f1a4d13c
Inmny <1342885270@qq.com>
2023-01-09 21:58:04 +0800

    Add CW_Trait

--- Code/CW_Library_Family.cs
+++ Code/CW_Library_Family.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Cultivation_Way.Library
 {
-    public class CW_Library_Family
+    public class CW_Asset_Family : Asset
+    {
+
+    }
+    public class CW_Library_Family : AssetLibrary<CW_Asset_Family>
     {
     }
 }
---
+++ Code/CW_Library_Trait.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivation_Way.Library
+{
+    public class CW_Asset_Trait : Asset
+    {
+        public ActorTrait origin_trait;
+        public CW_BaseStats addition_stats;
+        public WorldAction action_when_get;
+    }
+    public class CW_Library_Trait : AssetLibrary<CW_Asset_Trait>
+    {
+        public override void init()
+        {
+            base.init();
+            foreach(ActorTrait trait in AssetManager.traits.list)
+            {
+                add(new CW_Asset_Trait()
+                {
+                    id = trait.id,
+                    origin_trait = trait,
+                    addition_stats = new CW_BaseStats(),
+                    action_when_get = null
+                });
+            }
+        }
+    }
+}
