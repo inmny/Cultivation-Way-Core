c113790ad1ec7bd744dc172c0b7c650dace142e0
Inmny <1342885270@qq.com>
2023-01-10 13:39:19 +0800

    Structure of item

---
+++ Code/CW_Library_ItemAccessoryMaterial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivation_Way.Library
+{
+    public class CW_Library_ItemAccessoryMaterial : ItemAssetLibrary<CW_Asset_Item>
+    {
+        public override void init()
+        {
+            base.init();
+            foreach (ItemAsset asset in AssetManager.items_material_accessory.list)
+            {
+                __add(asset);
+            }
+        }
+        public override ItemAsset add(ItemAsset pAsset)
+        {
+            if (!AssetManager.items_material_accessory.list.Contains(pAsset)) AssetManager.items_material_accessory.add(pAsset);
+            return base.add(new CW_Asset_Item(pAsset));
+        }
+        public CW_Asset_Item add(ItemAsset asset, CW_BaseStats addition_stats)
+        {
+            if (!AssetManager.items_material_accessory.list.Contains(asset)) AssetManager.items_material_accessory.add(asset);
+            return __add(asset, addition_stats);
+        }
+        internal CW_Asset_Item __add(ItemAsset asset, CW_BaseStats addition_stats = null)
+        {
+            return (CW_Asset_Item)base.add(new CW_Asset_Item(asset, addition_stats == null ? new CW_BaseStats() : addition_stats));
+        }
+    }
+}
---
+++ Code/CW_Library_ItemAsset.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivation_Way.Library
+{
+    public class CW_Asset_Item : ItemAsset
+    {
+        public CW_BaseStats addition_stats;
+        public CW_Asset_Item() { }
+        public CW_Asset_Item(ItemAsset origin_asset)
+        {
+            this.id = origin_asset.id;
+            this.addition_stats = new CW_BaseStats();
+            this.attackAction = origin_asset.attackAction;
+            this.attackType = origin_asset.attackType;
+            this.baseStats = origin_asset.baseStats;
+            this.cached_sprite = origin_asset.cached_sprite;
+            this.cost_gold = origin_asset.cost_gold;
+            this.cost_resource_1 = origin_asset.cost_resource_1;
+            this.cost_resource_2 = origin_asset.cost_resource_2;
+            this.cost_resource_id_1 = origin_asset.cost_resource_id_1;
+            this.cost_resource_id_2 = origin_asset.cost_resource_id_2;
+            this.equipmentType = origin_asset.equipmentType;
+            this.equipment_value = origin_asset.equipment_value;
+            this.materials = origin_asset.materials;
+            this.minimum_city_storage_resource_1 = origin_asset.minimum_city_storage_resource_1;
+            this.mod_rank = origin_asset.mod_rank;
+            this.mod_translation = origin_asset.mod_translation;
+            this.mod_type = origin_asset.mod_type;
+            this.name_class = origin_asset.name_class;
+            this.name_templates = origin_asset.name_templates;
+            this.path_icon = origin_asset.path_icon;
+            this.pool = origin_asset.pool;
+            this.projectile = origin_asset.projectile;
+            this.quality = origin_asset.quality;
+            this.rarity = origin_asset.rarity;
+            this.slash = origin_asset.slash;
+            this.tech_needed = origin_asset.tech_needed;
+        }
+        public CW_Asset_Item(ItemAsset origin_asset, CW_BaseStats addition_stats)
+        {
+            this.id = origin_asset.id;
+            this.addition_stats = addition_stats;
+            this.attackAction = origin_asset.attackAction;
+            this.attackType = origin_asset.attackType;
+            this.baseStats = origin_asset.baseStats;
+            this.cached_sprite = origin_asset.cached_sprite;
+            this.cost_gold = origin_asset.cost_gold;
+            this.cost_resource_1 = origin_asset.cost_resource_1;
+            this.cost_resource_2 = origin_asset.cost_resource_2;
+            this.cost_resource_id_1 = origin_asset.cost_resource_id_1;
+            this.cost_resource_id_2 = origin_asset.cost_resource_id_2;
+            this.equipmentType = origin_asset.equipmentType;
+            this.equipment_value = origin_asset.equipment_value;
+            this.materials = origin_asset.materials;
+            this.minimum_city_storage_resource_1 = origin_asset.minimum_city_storage_resource_1;
+            this.mod_rank = origin_asset.mod_rank;
+            this.mod_translation = origin_asset.mod_translation;
+            this.mod_type = origin_asset.mod_type;
+            this.name_class = origin_asset.name_class;
+            this.name_templates = origin_asset.name_templates;
+            this.path_icon = origin_asset.path_icon;
+            this.pool = origin_asset.pool;
+            this.projectile = origin_asset.projectile;
+            this.quality = origin_asset.quality;
+            this.rarity = origin_asset.rarity;
+            this.slash = origin_asset.slash;
+            this.tech_needed = origin_asset.tech_needed;
+        }
+    }
+}
