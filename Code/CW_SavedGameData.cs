3eaa34173a7d18cef6719c267341e2ec9db956a0
Inmny <1342885270@qq.com>
2023-01-29 21:54:20 +0800

    拓展建筑数据，未实装

---
+++ Code/CW_SavedGameData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cultivation_Way
+{
+    [Serializable]
+    public class CW_SavedGameData
+    {
+        public int save_version;
+        public int world_width;
+        public int world_height;
+        public MapStats map_stats;
+        public WorldLaws world_laws;
+        public string tile_str;
+        public List<string> tile_map;
+        public int[][] tile_array;
+        public int[][] tile_amounts;
+        public List<int> fire = new List<int>();
+        public List<int> conway_eater = new List<int>();
+        public List<int> conway_creator = new List<int>();
+        public List<WorldTileData> tiles = new List<WorldTileData>();
+        public List<CW_CityData> cities = new List<CW_CityData>();
+        public List<ActorData> actor_datas = new List<ActorData>();
+        public List<CW_ActorData> cw_actor_datas = new List<CW_ActorData>();
+        public List
+    }
+}
