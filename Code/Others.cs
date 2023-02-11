5fa7bafa9963c2eea6fb43cbcdafa35403cd1f3a
Inmny <1342885270@qq.com>
2023-02-11 09:18:27 +0800

    调整武道修炼速度

--- Code/Others.cs
+++ Code/Others.cs
@@ -38,8 +38,8 @@ namespace Cultivation_Way.Others
         public const float chunk_wakan_level_spread_grad = 0.1f;
         public const uint cultisys_immortol_tag = 0x1;
         public const uint cultisys_bushido_tag = 0x2;
-        public const float bushido_force_culti_chance = 3f;
-        public const float bushido_force_culti_co = 0.01f;
+        public const float bushido_force_culti_chance = 2.5f;
+        public const float bushido_force_culti_co = 0.008f;
         public const float seconds_per_month = 3f;
         public const int yao_transform_level = 7;
         public const int special_body_create_level = 19;
