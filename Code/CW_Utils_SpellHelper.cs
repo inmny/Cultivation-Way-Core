fd4528327eb50a2748e4da7846b6996f8c259ead
Inmny <1342885270@qq.com>
2023-02-20 16:02:07 +0800

    置SkipIfShake=False

--- Code/CW_Utils_SpellHelper.cs
+++ Code/CW_Utils_SpellHelper.cs
@@ -21,12 +21,12 @@ namespace Cultivation_Way.Utils
             };
         public static bool is_enemy(BaseSimObject o_1, BaseSimObject o_2)
         {
-			if (o_1.kingdom == null || o_2.kingdom == null) return false;
+			if (o_1==null||o_2==null||o_1.kingdom == null || o_2.kingdom == null) return true;
 			return ((!o_1.kingdom.asset.mobs && !o_2.kingdom.asset.mobs) || !MapBox.instance.worldLaws.world_law_peaceful_monsters.boolVal) && o_2.kingdom.isEnemy(o_1.kingdom);
 		}
 		public static bool is_enemy(Kingdom k_1, Kingdom k_2)
 		{
-			if (k_1 == null || k_2 == null) return false;
+			if (k_1 == null || k_2 == null) return true;
 			return ((!k_1.asset.mobs && !k_2.asset.mobs) || !MapBox.instance.worldLaws.world_law_peaceful_monsters.boolVal) && k_2.isEnemy(k_1);
 		}
 		public static List<List<BaseSimObject>> find_kingdom_enemies_in_chunk(MapChunk pChunk, Kingdom pMainKingdom)
@@ -36,18 +36,19 @@ namespace Cultivation_Way.Utils
         }
 		internal static void __find_kingdom_enemies_in_chunk(MapChunk pChunk, Kingdom pMainKingdom)
 		{
-			if (pChunk.k_list_objects.Count == 0 || pMainKingdom == null) return;
 			if (pChunk == temp_list_objects_enemies_chunk && pMainKingdom == temp_list_objects_enemies_kingdom) return;
 
 			temp_list_objects_enemies_chunk = pChunk;
 			temp_list_objects_enemies_kingdom = pMainKingdom;
 
 			temp_list_objects_enemies.Clear();
-			int count = pChunk.k_list_objects.Count;
+            if (pChunk.k_list_objects.Count == 0) return;
+
+            int count = pChunk.k_list_objects.Count;
 			for (int i = 0; i < count; i++)
 			{
 				Kingdom kingdom = pChunk.k_list_objects[i];
-				if (kingdom != null && ((!kingdom.asset.mobs && !pMainKingdom.asset.mobs) || !MapBox.instance.worldLaws.world_law_peaceful_monsters.boolVal) && pMainKingdom.isEnemy(kingdom))
+				if (is_enemy(kingdom, pMainKingdom))
 				{
 					temp_list_objects_enemies.Add(pChunk.k_dict_objects[kingdom]);
 				}
@@ -198,11 +199,11 @@ namespace Cultivation_Way.Utils
 			if (target == null || !target.base_data.alive || (!ignore_user_alive && (user == null || !user.base_data.alive))) return false;
 			if (target.objectType == MapObjectType.Actor)
 			{
-				((CW_Actor)target).get_hit(damage, true, attack_type, user, true);
+				((CW_Actor)target).get_hit(damage, true, attack_type, user, false);
 			}
 			else if (target.objectType == MapObjectType.Building)
 			{
-				CW_Building.func_getHit((Building)target, damage, true, (AttackType)attack_type, user, true);
+				CW_Building.func_getHit((Building)target, damage, true, (AttackType)attack_type, user, false);
 			}
             return true;
 		}
