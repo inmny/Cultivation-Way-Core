d843a8128de4658a6bd9fea2887b3ee250003564
Inmny <1342885270@qq.com>
2023-03-12 23:03:31 +0800

    重新开始

--- Code/CW_ClassNick.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cultivation_Way
-{
-    using CW_CultiBook = Cultivation_Way.Library.CW_Asset_CultiBook;
-    using CW_ActorStats = Cultivation_Way.Library.CW_ActorStats;
-    using CW_CultiSys = Cultivation_Way.Library.CW_Asset_CultiSys;
-    using CW_Family = Cultivation_Way.Library.CW_Asset_Family;
-}
