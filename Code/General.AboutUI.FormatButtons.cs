a4022a7f28e00e670075043c2e7bb0e158de2e15
inmny <1342885270@qq.com>
2023-07-21 18:16:26 +0800

    添加多生物随机放置的按钮模板

--- Code/General.AboutUI.FormatButtons.cs
+++ Code/General.AboutUI.FormatButtons.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Cultivation_Way.UI;
 using NCMS.Utils;
 
@@ -25,6 +26,37 @@ public static class FormatButtons
         god_power.click_action = AssetManager.powers.spawnUnit;
         PowerButton button = CWTab.create_button(
             god_power.id, "ui/Icons/" + actor_asset.icon,
+            null, ButtonType.GodPower
+        );
+        CWTab.add_button(button);
+        return button;
+    }
+
+    /// <summary>
+    ///     添加一系列生物作为一个按钮到生物放置区域
+    /// </summary>
+    /// <param name="actor_ids">需要放置的生物的id</param>
+    /// <param name="name_locale">按钮名的key</param>
+    /// <param name="icon">图标名, 具体访问路径为$"ui/Icons/{icon}"</param>
+    /// <returns></returns>
+    public static PowerButton add_actors_button(List<string> actor_ids, string name_locale, string icon)
+    {
+        foreach (string actor_id in actor_ids)
+        {
+            if (AssetManager.actor_library.get(actor_id) == null)
+            {
+                Logger.Warn($" Please add actor button after ActorAsset for {actor_id} created");
+                return null;
+            }
+        }
+
+        GodPower god_power = AssetManager.powers.clone("spawn_" + name_locale, "_spawnActor");
+        god_power.name = name_locale;
+        god_power.actor_asset_ids = new List<string>();
+        god_power.actor_asset_ids.AddRange(actor_ids);
+        god_power.click_action = AssetManager.powers.spawnUnit;
+        PowerButton button = CWTab.create_button(
+            god_power.id, "ui/Icons/" + icon,
             null, ButtonType.GodPower
         );
         CWTab.add_button(button);
