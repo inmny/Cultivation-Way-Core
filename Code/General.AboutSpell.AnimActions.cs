bbe76275435f6417b79c8ad481181dab53fe12dc
inmny <1342885270@qq.com>
2023-07-24 19:25:12 +0800

    添加自动调整大小的AnimAction

--- Code/General.AboutSpell.AnimActions.cs
+++ Code/General.AboutSpell.AnimActions.cs
@@ -12,11 +12,6 @@ public static class AnimActions
     /// <summary>
     ///     使用者(src_obj), 目标(dst_obj), 按anim_type=ON_USER/ON_TARGET, 设定src_vec,dst_vec = target_vec / user_vec
     /// </summary>
-    /// <param name="spell_asset"></param>
-    /// <param name="user"></param>
-    /// <param name="target"></param>
-    /// <param name="target_tile"></param>
-    /// <param name="cost"></param>
     public static void simple_on_something(CW_SpellAsset spell_asset, BaseSimObject user, BaseSimObject target,
         WorldTile target_tile, float cost)
     {
@@ -28,6 +23,30 @@ public static class AnimActions
             SpellAnimType.ON_TARGET => CW_Core.mod_state.anim_manager.spawn_anim(spell_asset.anim_id,
                 target == null ? target_tile.posV : target.currentPosition,
                 target == null ? target_tile.posV : target.currentPosition, user, target),
+            _ => throw new Exception(
+                $"simple_on_obj: anim_type {spell_asset.anim_type} not supported for {spell_asset.id}")
+        };
+
+        if (anim is not { isOn: true }) return;
+        anim.data.set(DataS.spell_cost, cost);
+    }
+
+    /// <summary>
+    ///     使用者(src_obj), 目标(dst_obj), 按anim_type=ON_USER/ON_TARGET, 设定src_vec,dst_vec = target_vec / user_vec
+    ///     <para>自动调整为目标大小</para>
+    /// </summary>
+    public static void on_something_auto_scale(CW_SpellAsset spell_asset, BaseSimObject user, BaseSimObject target,
+        WorldTile target_tile, float cost)
+    {
+        if (string.IsNullOrEmpty(spell_asset.anim_id)) return;
+        Animation.SpriteAnimation anim = spell_asset.anim_type switch
+        {
+            SpellAnimType.ON_USER => CW_Core.mod_state.anim_manager.spawn_anim(spell_asset.anim_id,
+                user.currentPosition, user.currentPosition, user, target, user.stats[S.scale]),
+            SpellAnimType.ON_TARGET => CW_Core.mod_state.anim_manager.spawn_anim(spell_asset.anim_id,
+                target == null ? target_tile.posV : target.currentPosition,
+                target == null ? target_tile.posV : target.currentPosition, user, target,
+                target == null ? 1f : target.stats[S.scale]),
             _ => throw new Exception(
                 $"simple_on_obj: anim_type {spell_asset.anim_type} not supported for {spell_asset.id}")
         };
