47439f9a4e3c8994c83fddaa8a18888764667d4a
inmny <1342885270@qq.com>
2023-07-31 20:28:32 +0800

    添加获取WorldTile对应EnergyMapTile的拓展函数

---
+++ Code/Extension.WorldTileTools.cs
@@ -0,0 +1,11 @@
+using Cultivation_Way.Core;
+
+namespace Cultivation_Way.Extension;
+
+public static class WorldTileTools
+{
+    public static CW_EnergyMapTile get_energy_tile(this WorldTile tile, string energy_id)
+    {
+        return CW_Core.mod_state.map_chunk_manager.maps[energy_id].map[tile.x, tile.y];
+    }
+}
