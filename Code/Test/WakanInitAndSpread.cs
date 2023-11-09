bab13595dba45c49b43e6056bb4aff401eeea8dc
inmny <1342885270@qq.com>
2023-11-09 21:43:33 +0800

    Repo: 添加测试工具

---
+++ Code/Test/WakanInitAndSpread.cs
@@ -0,0 +1,20 @@
+using Cultivation_Way.Core;
+
+namespace Cultivation_Way.Test;
+
+internal static class WakanInitAndSpread
+{
+    public static void ClearWakan()
+    {
+        CW_EnergyMap map = CW_Core.mod_state.energy_map_manager.maps[Implementation.Content_Constants.energy_wakan_id];
+        
+        for (int i = 0; i < map.map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.map.GetLength(1); j++)
+            {
+                map.map[i, j].value = 0;
+                map.map[i, j].update(map.energy);
+            }
+        }
+    }
+}
