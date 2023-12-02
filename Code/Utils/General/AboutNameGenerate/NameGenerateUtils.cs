7e7e8ed5a78cc45e8cff691fe91d9f0f18f90072
inmny <1342885270@qq.com>
2023-12-03 00:17:19 +0800

    Optim: 优化功法和物品名生成逻辑(避免参数重复获取)

--- Code/Utils/General/AboutNameGenerate/NameGenerateUtils.cs
+++ Code/Utils/General/AboutNameGenerate/NameGenerateUtils.cs
@@ -1,59 +1,38 @@
+using System.Collections.Generic;
+#if 一米_中文名
 using Chinese_Name;
+#endif
 using Cultivation_Way.Core;
 using Cultivation_Way.Library;
 using Cultivation_Way.Others;
+using NeoModLoader.api.attributes;
 
 namespace Cultivation_Way.General.AboutNameGenerate;
 
 public static class NameGenerateUtils
 {
 #if 一米_中文名
+    [Hotfixable]
     public static string GenerateCultibookName(Cultibook pCultibook, CW_Actor pEditor)
     {
         CN_NameGeneratorAsset generator = CN_NameGeneratorLibrary.Get("cultibook_name");
 
-        GetCultibookNameParameters para_getter =
-            ParameterGetters.GetCustomParameterGetter<GetCultibookNameParameters>(generator.parameter_getter);
-        int max_try = 10;
-        while (max_try-- > 0)
-        {
-            CN_NameTemplate template = generator.GetRandomTemplate();
-            var parameters = template.GetParametersToFill();
-            
-            para_getter(pCultibook, pEditor, parameters);
-            
-            string name = template.GenerateName(parameters);
-            
-            if(string.IsNullOrWhiteSpace(name)) continue;
-
-            return name;
-        }
+        var para = new Dictionary<string, string>();
+        ParameterGetters.GetCustomParameterGetter<GetCultibookNameParameters>(generator.parameter_getter)(pCultibook,
+            pEditor, para);
         
-        return "";
+        return generator.GenerateName(para);
     }
-
+    [Hotfixable]
     public static string GenerateItemName(CW_ItemData pItemData, CW_ItemAsset pItemAsset, CW_Actor pCreator)
     {
-        CN_NameGeneratorAsset generator = CN_NameGeneratorLibrary.Get(pItemAsset.VanillaAsset.name_class);
+        CN_NameGeneratorAsset generator = CN_NameGeneratorLibrary.Get(pItemAsset.GetTypeName());
 
-        GetCWItemNameParameters para_getter =
-            ParameterGetters.GetCustomParameterGetter<GetCWItemNameParameters>(generator.parameter_getter);
-        int max_try = 10;
-        while (max_try-- > 0)
-        {
-            CN_NameTemplate template = generator.GetRandomTemplate();
-            var parameters = template.GetParametersToFill();
-            
-            para_getter(pItemData, pItemAsset, pCreator, parameters);
-            
-            string name = template.GenerateName(parameters);
-            
-            if(string.IsNullOrWhiteSpace(name)) continue;
+        var para = new Dictionary<string, string>();
+        ParameterGetters.GetCustomParameterGetter<GetCWItemNameParameters>(generator.parameter_getter)(pItemData,
+            pItemAsset, pCreator, para);
 
-            return name;
-        }
-        
-        return "";
+        return generator.GenerateName(para);
     }
 #else
     public static string GenerateCultibookName(Cultibook pCultibook, CW_Actor pCreator, CW_Actor pEditor)
