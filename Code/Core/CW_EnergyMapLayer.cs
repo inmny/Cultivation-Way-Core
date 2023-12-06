318492e6a3fa7b3e91fc3751542330b140de583f
inmny <1342885270@qq.com>
2023-12-06 14:04:51 +0800

    Prepare: Prepare for new energy map layer system

---
+++ Code/Core/CW_EnergyMapLayer.cs
@@ -0,0 +1,9 @@
+namespace Cultivation_Way.Core;
+
+public class CW_EnergyMapLayer : MapLayer
+{
+    public override void UpdateDirty(float pElapsed)
+    {
+        base.UpdateDirty(pElapsed);
+    }
+}
