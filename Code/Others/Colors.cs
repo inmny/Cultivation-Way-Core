1e4f7813cf043690876158a7a0a0bf94ac449644
inmny <1342885270@qq.com>
2023-12-14 08:42:42 +0800

    Adjust: 开放颜色访问

--- Code/Others/Colors.cs
+++ Code/Others/Colors.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Cultivation_Way.Others;
 
-internal static class Colors
+public static class Colors
 {
     public static readonly Color default_color = new(1, 0.6073f, 0.1103f, 1);
 }
