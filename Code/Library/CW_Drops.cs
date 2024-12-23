bde660d4e5252c1a3ffff2b4dd5fccf38810e8af
inmny <1342885270@qq.com>
2024-12-24 00:47:28 +0800

    更新功法命名和相关词库

--- Code/Library/CW_Drops.cs
+++ Code/Library/CW_Drops.cs
@@ -29,7 +29,7 @@ internal static class CW_Drops
                     return;
                 EnergyAsset energy_asset = Manager.energies.get(CW_Core.mod_state.energy_map_manager.current_map_id);
                 CW_EnergyMapTile energy_tile = tile.GetEnergyTile(energy_asset.id);
-                float scale = CW_Core.Instance.GetConfig()["worldlaw_energy_grid"]["energy_change_scale"].FloatVal;
+                float scale = CW_Core.Instance.GetConfig()["worldlaw_energy_grid"]["energy_change_scale"].IntVal;
 
                 float new_value = Mathf.Min((energy_tile.value + scale) * 1.1f * scale, 1e15f);
 
